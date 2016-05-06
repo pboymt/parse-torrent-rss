fe965000b7c180a08c1052a2b931ac6719fc53cf
pboymt <pboymt@foxmail.com>
2016-05-06 13:16:45 +0800

    a

--- index.js
+++ index.js
@@ -5,66 +5,97 @@ var parseXML = xml2js.parseString;
 var http = require('http');
 var responseText = '';
 var xmlObj = {};
+var downloadedNum = 0;
 var resources = {
-    'nyaa': {
-        'url': 'http://www.nyaa.se/?page=rss',
-        'dir': 'nyaa/'
+    nyaa: {
+        url: 'http://www.nyaa.se/?page=rss',
+        dir: 'nyaa/',
+        run: function() {
+            if (!fs.existsSync('nyaa/')) {
+                fs.mkdirSync('nyaa/');
+            }
+            var roundDownload = function(list, which) {
+                console.log('正在下载第' + which + '个');
+                downloadTorrent(list[which]['link'][0], list[which]['title'] + '.torrent', 'nyaa/', function(isDown) {
+                    if (isDown) {
+                        downloadedNum++;
+                    }
+                    if (which >= list.length - 1) {
+                        console.log('下载完毕！下载了' + downloadedNum + '个文件');
+                    } else {
+                        roundDownload(list, which + 1);
+                    }
+                });
+            };
+            http.get('http://www.nyaa.se/?page=rss', function(res) {
+                responseText = '';
+                res.on('data', function(chunk) {
+                    //console.log('BODY: ' + chunk);
+                    responseText += chunk;
+                });
+                res.on('end', function() {
+                    //console.log(responseText);
+                    fs.writeFile('nyaa/' + 'result.xml', responseText);
+                    parseXML(responseText, function(err, result) {
+                        console.dir(result['rss']['channel'][0]['item'].length);
+                        fs.writeFile('nyaa/' + 'result.json', JSON.stringify(result));
+                        var items = result['rss']['channel'][0]['item'];
+                        roundDownload(items, 0);
+                    });
+                });
+            }).on('error', function() {
+                console.log('Error');
+            });
+        }
+    },
+    dmhy: {
+        url: '',
+        dir: 'dmhy/main/',
+        run: function() {
+            if (!fs.existsSync('dmhy/')) {
+                fs.mkdirSync('dmhy/');
+            }
+            if (!fs.existsSync('dmhy/main/')) {
+                fs.mkdirSync('dmhy/main/');
+            }
+        }
     }
 };
-var downloadTorrent = function(url, filename, callback) {
+var downloadTorrent = function(url, filename, dir, callback) {
     //console.log(url);
     filename = filename.replace(/\//, '-');
-    http.get(url, function(res) {
-        var writeStream = fs.createWriteStream(resources['nyaa']['dir'] + filename);
-        writeStream.on('finish', function() {
-            console.log('文件' + filename + '下载完成');
+    if (fs.existsSync(dir + filename)) {
+        console.log('文件 "' + filename + '" 已存在');
+        callback(false);
+    } else {
+        http.get(url, function(res) {
+            var writeStream = fs.createWriteStream(dir + filename);
+            writeStream.on('finish', function() {
+                console.log('文件 "' + filename + '" 下载完成');
+            });
+            res.on('data', function(chunk) {
+                writeStream.write(chunk);
+            });
+            res.on('end', function() {
+                writeStream.end();
+                callback(true);
+            });
         });
-        res.on('data', function(chunk) {
-            writeStream.write(chunk);
-        });
-        res.on('end', function() {
-            writeStream.end();
-            callback();
-        });
-    });
+    }
 };
-var roundDownload = function(list, which) {
-    console.log('正在下载第' + which + '个');
-    downloadTorrent(list[which]['link'][0], list[which]['title'] + '.torrent', function() {
-        if (which >= list.length - 1) {
-            console.log('下载完毕！');
-        } else {
-            roundDownload(list, which + 1);
-        }
-    });
-};
-
-http.get(resources['nyaa']['url'], function(res) {
-    responseText = '';
-    res.on('data', function(chunk) {
-        //console.log('BODY: ' + chunk);
-        responseText += chunk;
-    });
-    res.on('end', function() {
-        //console.log(responseText);
-        fs.writeFile(resources['nyaa']['dir'] + 'result.xml', responseText);
-        parseXML(responseText, function(err, result) {
-            console.dir(result['rss']['channel'][0]['item'].length);
-            fs.writeFile(resources['nyaa']['dir'] + 'result.json', JSON.stringify(result));
-            var items = result['rss']['channel'][0]['item'];
-            roundDownload(items, 0);
-        });
-    });
-}).on('error', function() {
-    console.log('Error');
-});
+if (process.argv[2]) {
+    resources[process.argv[2]].run();
+} else {
+    resources['nyaa'].run();
+}
+//console.log(process.argv[2]);
 // fs.readFile('result.xml', function(err, data) {
 //     //console.log(data.toString());
 //     parseXML(data.toString(), function(err, result) {
 //         console.dir(result['rss']['channel'][0]['item'].length);
-//         //fs.writeFile(resources['nyaa']['dir'] + 'result.json', JSON.stringify(result));
+//         //fs.writeFile('nyaa/' + 'result.json', JSON.stringify(result));
 //         var items = result['rss']['channel'][0]['item'];
 //         roundDownload(items, 0);
-//         //downloadTorrent(items[x]['link'][0], resources['nyaa']['dir'] + items[x]['title'] + '.torrent');
+//         //downloadTorrent(items[x]['link'][0], 'nyaa/' + items[x]['title'] + '.torrent');
 //     });
 // });
