38ed153cd3c25980482c8f88b579a9b5d3a75ae5
pboymt <pboymt@foxmail.com>
2016-05-10 00:16:32 +0800

    ……

--- index.js
+++ index.js
@@ -65,11 +65,17 @@ var resources = {
             if (!fs.existsSync('dmhy/main/' + new Date().getFullYear() + '/')) {
                 fs.mkdirSync('dmhy/main/' + new Date().getFullYear() + '/');
             }
-            if (!fs.existsSync('dmhy/main/' + new Date().getFullYear() + '/' + (new Date().getMonth() * 1 + 1) + '/')) {
-                fs.mkdirSync('dmhy/main/' + new Date().getFullYear() + '/' + (new Date().getMonth() * 1 + 1) + '/');
+            if (!fs.existsSync('dmhy/main/' + new Date().getFullYear() + '/' +
+                    (Array(2).join(0) + (new Date().getMonth() * 1 + 1)).slice(-2) + '/')) {
+                fs.mkdirSync('dmhy/main/' + new Date().getFullYear() + '/' +
+                    (Array(2).join(0) + (new Date().getMonth() * 1 + 1)).slice(-2) + '/');
             }
-            if (!fs.existsSync('dmhy/main/' + new Date().getFullYear() + '/' + (new Date().getMonth() * 1 + 1) + '/' + new Date().getDate() + '/')) {
-                fs.mkdirSync('dmhy/main/' + new Date().getFullYear() + '/' + (new Date().getMonth() * 1 + 1) + '/' + new Date().getDate() + '/');
+            if (!fs.existsSync('dmhy/main/' + new Date().getFullYear() + '/' +
+                    (Array(2).join(0) + (new Date().getMonth() * 1 + 1)).slice(-2) + '/' +
+                    (Array(2).join(0) + new Date().getDate()).slice(-2) + '/')) {
+                fs.mkdirSync('dmhy/main/' + new Date().getFullYear() + '/' +
+                    (Array(2).join(0) + (new Date().getMonth() * 1 + 1)).slice(-2) + '/' +
+                    (Array(2).join(0) + new Date().getDate()).slice(-2) + '/');
             }
             var roundDownload = function(list, which) {
                 console.log('正在下载第' + which + '个');
@@ -100,9 +106,9 @@ var resources = {
                             delete result['rss']['channel'][0]['item'][i]['description'];
                             let hash = result['rss']['channel'][0]['item'][i]['enclosure'][0]['$']['url'].match(/[2-7A-Z]{32}/);
                             let d = new Date(Date.parse(result['rss']['channel'][0]['item'][i]['pubDate']));
-                            let dpath = (d.getFullYear() + '/' +
+                            let dpath = d.getFullYear() + '/' +
                                 (Array(2).join(0) + (d.getMonth() * 1 + 1)).slice(-2) + '/' +
-                                (Array(2).join(0) + d.getDate()).slice(-2)) + "/";
+                                (Array(2).join(0) + d.getDate()).slice(-2) + '/';
                             result['rss']['channel'][0]['item'][i]['dpath'] = dpath;
                             result['rss']['channel'][0]['item'][i]['link'] = "http://dl.dmhy.org/" + dpath + base32to16(hash[0]) + ".torrent";
                             delete result['rss']['channel'][0]['item'][i]['enclosure'];
